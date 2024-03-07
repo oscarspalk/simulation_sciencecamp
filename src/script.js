54f5ae14d62ff920032404211377404b2fb14602
oscarspalk <oscar.gaardsted.spalk@gmail.com>
2024-03-07 16:07:47 +0100

    added planets

--- src/script.js
+++ src/script.js
@@ -2,7 +2,14 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const loader = new THREE.FileLoader();
+
+const planetsJSON = await loader.loadAsync("planets.json")
+const planets = JSON.parse(planetsJSON)
+const planetModels = []
+console.log(planets)
 // Debug
 const gui = new dat.GUI()
 
@@ -12,24 +19,34 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+scene.background = new THREE.Color(0x333333)
 
-// Materials
+let reference = null
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+var gltfLoader = new GLTFLoader();
+for(let i = 0; i < planets.length; i++){
+    
+    let planet = planets[i]
+    let planetName = planet.name
+    let planetScene = await gltfLoader.loadAsync(`planets/${planetName}.glb`)
+    let model = planetScene.scene.children[0]
+    if(reference == null){
+        reference = planet.radius
+    }
+    else {
+        let multiplier = planet.radius/reference
+        //model.scale.set(multiplier, multiplier, multiplier)
+    }
+    console.log(model)
+    model.position.set(i*2500,0,0)
+    planetModels.push(model)
+    scene.add(model)
+}
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.AmbientLight(0xffffff, 3.0)
 scene.add(pointLight)
 
 /**
@@ -59,9 +76,9 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 100, 100000)
+camera.position.x = 100
+camera.position.y = 100
 camera.position.z = 2
 scene.add(camera)
 
@@ -82,6 +99,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+var controls = new OrbitControls( camera, renderer.domElement ); 
+
 const clock = new THREE.Clock()
 
 const tick = () =>
@@ -90,7 +109,6 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
