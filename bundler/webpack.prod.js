3e31b795aa6c419b298806075c3b4c79338f9508
oscarspalk <oscar.gaardsted.spalk@gmail.com>
2024-03-08 13:09:08 +0100

    toplevel await

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -5,6 +5,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = merge(
     commonConfiguration,
     {
+        experiments: {
+            topLevelAwait: true
+        },
         mode: 'production',
         entry: ['@babel/polyfill', './src/script.js'],
         plugins:
